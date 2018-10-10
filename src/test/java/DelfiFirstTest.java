557dbb873df0e6a62bdce9944ab6719b2ecbd19a
ievabusa <ievaa.bussa@gmail.com>
2018-10-10 20:57:42 +0300

    Im commiting message

---
+++ src/test/java/DelfiFirstTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DelfiFirstTest {
+    private final By ARTICLE_TITLE = By.xpath(".//a[@class = 'top2012-title']");
+    @Test
+    public void delfiFirstTitleTest() {
+        System.setProperty("webdriver.gecko.driver", "/Users/ievabusa/Downloads/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("Http:delfi.lv");
+
+        String articleToCheck = "'OlyBet' basketbola līga: LU - 'Kalev/Cramo'. Video tiešraide";
+
+        WebElement article = driver.findElement(ARTICLE_TITLE);
+
+        String articleText = article.getText();
+
+        Assertions.assertEquals(articleToCheck, articleText, "Fatal error");
+
+
+
+
+    }
+
+
+
+}
