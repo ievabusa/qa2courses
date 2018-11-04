29ef74b1b9d010cce04aea63c4e7c19fc92e148c
ievabusa <ievaa.bussa@gmail.com>
2018-11-04 17:13:55 +0200

    QA2Homework2

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -3,21 +3,24 @@ import org.junit.jupiter.api.Test;
 
 public class HomeWork {
     private double loanAmount = 1000;
-    private double amountToPay = 5100;
+    private double amountToPay = 5400;
 
     @Test
     public void myTest() {
 
         double sum, sum1, sum2, sum3;
 
-        sum1 = (loanAmount * (1 + 10 * 0.1));
-        sum2 = (loanAmount * (1 + 10 * 0.08));
-        sum3 = (loanAmount * (1 + 10 * 0.06));
+        sum1 = myTest2(0.1);
+        sum2 = myTest2(0.08);
+        sum3 = myTest2(0.06);
 
-         sum = sum1 + sum2 + sum3;
+        sum = sum1 + sum2 + sum3;
 
-        Assertions.assertEquals(amountToPay,sum,"amountToPay is incorrect!");
+        Assertions.assertEquals(amountToPay, sum, "amountToPay is incorrect!");
+    }
 
+    private double myTest2(double percent) {
 
+        return loanAmount * (1 + 10 * percent);
     }
 }
